279f2017efb752b5c7318cc6e88b35af09f13d99
vinaka <ky@vinakanoiMac.local>
2021-06-29 00:17:18 +0900

    fetch API

--- src/components/ApiFetch.js
+++ src/components/ApiFetch.js
@@ -1,12 +1,17 @@
 import React,{useState, useEffect} from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 
 const ApiFetch = () => {
     const [posts, setPosts] = useState([])
     useEffect(() =>{
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(res =>{
-            setPosts(res.data)
+        // axios.get('https://jsonplaceholder.typicode.com/posts')
+        // .then(res =>{
+        //     setPosts(res.data)
+        // }
+        fetch('https://jsonplaceholder.typicode.com/posts',{method: 'GET'})
+        .then(res => res.json())
+        .then(data => {
+            setPosts(data)
         })
     },[])
         return (

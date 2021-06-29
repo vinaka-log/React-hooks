d206732a2ef5dbdc90bbc7c194402d827ae859a1
vinaka <ky@vinakanoiMac.local>
2021-06-29 23:48:49 +0900

    useContext

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import './App.css';
 // import BasicUseEffect from './components/BasicUseEffect';
 // import TimerContainer from './components/TimerContainer';
 import ApiFetch from './components/ApiFetch';
+import AppContext from './contexts/AppContext';
+import B from './components/B';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <Basic2 />
-        <BasicUseEffect /> */}
-        {/* <TimerContainer /> */}
-        <ApiFetch />
-      </header>
-    </div>
+    <AppContext.Provider value={'value from App.js'}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <Basic2 />
+          <BasicUseEffect /> */}
+          {/* <TimerContainer /> */}
+          <ApiFetch />
+          <B />
+
+        </header>
+      </div>
+    </AppContext.Provider>
   );
 }
 export default App;
---
+++ src/components/B.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import C from './C'
+
+const B = () => {
+    return (
+        <div>
+            <div>
+                <h3>
+                    B
+                </h3>
+                <C />
+            </div>
+        </div>
+    )
+}
+
+    export default B
+    

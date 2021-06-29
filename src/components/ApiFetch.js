26ecc846a523308442b8b001b0dab570e3e17821
vinaka <ky@vinakanoiMac.local>
2021-06-29 23:25:27 +0900

    fetch API ID

--- src/components/ApiFetch.js
+++ src/components/ApiFetch.js
@@ -3,24 +3,37 @@ import React,{useState, useEffect} from 'react'
 
 const ApiFetch = () => {
     const [posts, setPosts] = useState([])
+    const [id, setId] = useState(1)
+    const [clicked, setClicked] = useState(false)
+    const handlerClicked = () => {
+        setClicked(!clicked)
+    }
+
     useEffect(() =>{
         // axios.get('https://jsonplaceholder.typicode.com/posts')
         // .then(res =>{
         //     setPosts(res.data)
         // }
-        fetch('https://jsonplaceholder.typicode.com/posts',{method: 'GET'})
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}` ,{method: 'GET'})
         .then(res => res.json())
         .then(data => {
             setPosts(data)
         })
-    },[])
-        return (
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[clicked])
+
+    return (
         <div>
-            <ul>
+            <input type='text' value={id} onChange={evt=>setId(evt.target.value)} />
+            <br />
+            <button type='button' onClick={handlerClicked}>Get post</button>
+            <br />
+            {posts.title}
+            {/* <ul>
                 {
                     posts.map(post => <li key={post.id}> {post.title}</li>)
                 }
-            </ul>
+            </ul> */}
         </div>
     )
 }
